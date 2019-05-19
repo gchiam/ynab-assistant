eef4a4929d54d0e08c8818d2398abbb8eda8f85d
Gordon Chiam <gordon.chiam@gmail.com>
2019-05-19 19:11:09 +0800

    Implement a simple callback for the welcome intent

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,51 @@
+'use strict';
+
+// Import the Dialogflow module from the Actions on Google client library.
+const {
+  dialogflow,
+  Permission
+} = require('actions-on-google');
+
+// Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = dialogflow({ debug: true });
+
+
+// Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'.
+app.intent('welcome', (conv) => {
+  // Respond with the user's lucky number and end the conversation.
+  const name = conv.user.storage.userName;
+  if (!name) {
+    // Asks the user's permission to know their name, for personalization.
+    conv.ask('Welcome to YNAB Assistant');
+    conv.ask(new Permission({
+      context: 'Hi there, to get to know you better',
+      permissions: 'NAME',
+    }));
+  } else {
+    conv.ask(`Hi again, ${name}. Welcome to YNAB Assistant.`);
+    conv.close('I am still learning. Talk to you next time!');
+  }
+});
+
+
+// Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
+// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
+app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+  if (!permissionGranted) {
+    // If the user denied our request, go ahead with the conversation.
+    conv.ask(`OK, no worries. Welcome to YNAB Assistant`);
+  } else {
+    // If the user accepted our request, store their name in
+    // the 'conv.user.storage' object for the duration of the conversation.
+    conv.user.storage.userName = conv.user.name.display;
+    conv.ask(`Thanks, ${conv.user.storage.userName}. Welcome to YNAB Assistant`);
+  }
+  conv.close('I am still learning. Talk to you next time!');
+});
+
+
+// Set the DialogflowApp object to handle the HTTPS POST request.
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
